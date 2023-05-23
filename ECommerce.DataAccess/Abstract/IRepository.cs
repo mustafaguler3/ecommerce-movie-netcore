0bd79e233dab2953447a07e024b50b2dd449c5a6
mustafaguler3 <mustafa.glr@hotmail.com>
2023-05-23 22:12:49 +0300

    added repository pattern

---
+++ ECommerce.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ECommerce.DataAccess.Abstract
+{
+	public interface IRepository<T> where T : class
+	{
+		IEnumerable<T> GetAll();
+		T Get(Expression<Func<T, bool>> filter);
+		void Add(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entities);
+	}
+}
+
---
+++ ECommerce.DataAccess/Concrete/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+using ECommerce.DataAccess.Abstract;
+using ECommerce.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.DataAccess.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly VtContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(VtContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
+
