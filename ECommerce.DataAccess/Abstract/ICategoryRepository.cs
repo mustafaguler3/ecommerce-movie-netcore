cd94db7a98f973c98a34f150c4bf8cbd447ac57d
mustafaguler3 <mustafa.glr@hotmail.com>
2023-05-23 22:30:48 +0300

    refactoring category controller with repository

---
+++ ECommerce.DataAccess/Abstract/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ECommerce.Entity.Entity;
+
+namespace ECommerce.DataAccess.Abstract
+{
+	public interface ICategoryRepository : IRepository<Category>
+	{
+		void Update(Category category);
+		void Save();
+	}
+}
+
---
+++ ECommerce.DataAccess/Concrete/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+using ECommerce.DataAccess.Abstract;
+using ECommerce.DataAccess.Data;
+using ECommerce.Entity.Entity;
+
+namespace ECommerce.DataAccess.Concrete
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly VtContext _context;
+
+        public CategoryRepository(VtContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+    }
+}
+
